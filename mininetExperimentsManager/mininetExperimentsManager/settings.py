fc16d8ef2c12b823694cd42fc8b28e9d2afe67ce
Renan Rodrigues <renan.rodrigues@pinkapp.com>
2021-08-07 19:50:59 -0300

    creating sample of version screen

--- mininetExperimentsManager/mininetExperimentsManager/settings.py
+++ mininetExperimentsManager/mininetExperimentsManager/settings.py
@@ -119,11 +119,23 @@ USE_L10N = True
 
 USE_TZ = True
 
+LANGUAGES = [
+  ('en-us', 'English'),
+  ('pt-br', 'Brazilian Portuguese'),
+]
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
