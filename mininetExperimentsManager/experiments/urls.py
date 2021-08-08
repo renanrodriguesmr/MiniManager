5b095153a1a221ed0e8a01c6bd2f936624cb1d5e
Renan Rodrigues <renan.rodrigues@pinkapp.com>
2021-08-07 22:31:25 -0300

    redirecting to round screen

--- mininetExperimentsManager/experiments/urls.py
+++ mininetExperimentsManager/experiments/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('versao', views.version, name='versao'),
+    path('versao', views.version, name='version'),
+    path('rodada', views.round, name='round'),
 ]
--- mininetExperimentsManager/experiments/views.py
+++ mininetExperimentsManager/experiments/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def version(request):
     return render(request, 'version.html')
+
+def round(request):
+    if request.method == 'POST':
+        return HttpResponseRedirect(reverse('round'))
+
+    return render(request, 'round.html')
