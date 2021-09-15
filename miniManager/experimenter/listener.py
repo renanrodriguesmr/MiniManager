acaee24fe3c26e4120f939888629d4990afed0fe
renanrodriguesmr <renan.rodriguesmr@gmail.com>
2021-09-14 23:49:25 -0300

    fixing style of round page

--- miniManager/experimenter/listener.py
+++ miniManager/experimenter/listener.py
@@ -1,7 +1,9 @@
 from asyncio import run
+from django.utils.timezone import now
 from mininetWifiAdapter import IResultEventListener
 from .webSocketServer import WebSocketServer
 from .experimentsQueue import ExperimentsQueue
+from .models import Round
 
 
 class ExperimentListener(IResultEventListener):
@@ -12,8 +14,13 @@ class ExperimentListener(IResultEventListener):
         run(self.__sendMessage(subject))
 
         if subject["type"] == "UPDATE":
-            queue = ExperimentsQueue.instance()
-            queue.enable()
+            ExperimentsQueue.instance().enable()
+
+        if subject["type"] == "FINISH":
+            round = Round.objects.get(id=self.__roundID)
+            round.end = now()
+            round.status = Round.DONE
+            round.save()
 
     async def __sendMessage(self, subject):
         await WebSocketServer.sendMessageToRoom(self.__roundID, subject)
