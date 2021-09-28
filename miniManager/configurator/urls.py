13580bf90ee39b0a6790db49ebf7eebe07c0c9c0
renanrodriguesmr <renan.rodriguesmr@gmail.com>
2021-09-27 23:20:41 -0300

    fixing issues to save a new configuration

--- miniManager/configurator/urls.py
+++ miniManager/configurator/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import VersionView, VersionsView, TestPlanView, TestPlansView, ConfigurationView
+from .views import VersionView, VersionsView, TestPlanView, TestPlansView
 
 
 
 urlpatterns = [
-    path('configuration/', ConfigurationView.as_view(), name='configuration'),
     path('version/<test_plan_id>', VersionView.as_view(), name='version'),
     path('version/', VersionView.as_view(), name='version'),
     path('versions/<test_plan_id>', VersionsView.as_view(), name='versions'),
