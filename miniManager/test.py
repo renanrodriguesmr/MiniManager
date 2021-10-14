6112473e9bb6a42924770ba3463e26767e1aa8c3
renanrodriguesmr <renan.rodriguesmr@gmail.com>
2021-10-13 23:36:36 -0300

    remove unnecessary files

--- miniManager/test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-if __name__ == '__main__':
-    os.system("sudo python telemetry_mod7.py")
-    os.system("sudo mn -c")
-    os.system("sudo python telemetry_mod7.py")
-    os.system("sudo mn -c")
